040589cd35808e932cee1a561713f0a3a857c35d
imaravelias <giannismaravelias@yahoo.com>
2025-01-24 17:12:43 +0200

    Added child component for applying pagination on different components

---
+++ src/app/pagination/pagination.component.ts
@@ -0,0 +1,54 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-pagination',
+  standalone: false,
+
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.css'
+})
+
+
+
+export class PaginationComponent implements OnInit {
+
+
+  @Output() childEvent = new EventEmitter<number>(); //emit back the index once updated
+
+  @Input() childPagesSum: any //ok
+  @Input() childPageIndex: any //ok
+
+
+
+
+  ngOnInit(): void {
+    this.childEvent.emit(this.childPageIndex);
+  }
+
+
+
+  //pagination
+  selectPage(selectedPage: number) {
+    this.childPageIndex = selectedPage
+    this.ngOnInit()
+  }
+
+
+
+  pageBack() {
+    if (this.childPageIndex > 1)
+      this.childPageIndex = this.childPageIndex - 1
+    this.ngOnInit()
+  }
+
+
+
+  pageNext() {
+    if (this.childPageIndex < this.childPagesSum)
+      this.childPageIndex = this.childPageIndex + 1
+    this.ngOnInit()
+  }
+
+
+}
