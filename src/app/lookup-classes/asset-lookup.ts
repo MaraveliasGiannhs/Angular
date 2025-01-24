3e720ae17c37181e4caff51739648df69eb102fe
imaravelias <giannismaravelias@yahoo.com>
2025-01-24 17:13:13 +0200

    Added required fields for pagination

--- src/app/lookup-classes/asset-lookup.ts
+++ src/app/lookup-classes/asset-lookup.ts
@@ -3,4 +3,8 @@ import { AssetType } from "../Models/asset-type"
 export interface AssetLookup{
     id?: string 
     like: string
+    pageIndex: number
+    itemsPerPage: number //match names to work with api
+    orderItem:string
+    ascendingOrder:boolean
 }
